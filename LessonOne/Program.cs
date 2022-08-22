00d7dc7f7a5365be6b61328e18a7eb5e623002ed
talistalis <tsarst@gmail.com>
2022-08-22 22:17:36 +0300

    next lesson

--- LessonOne/Program.cs
+++ LessonOne/Program.cs
@@ -6,12 +6,32 @@ namespace LessonOne
     {
         static void Main(string[] args)
         {
-            int[] myArray = { 10, 3, 2, 55};
+            int[,] myArray = new int[10, 6];
 
-            for (int i = 0; i < myArray.Length; i++)
+            Random random = new Random();
+
+            int heght = myArray.GetLength(0);
+
+            int width = myArray.GetLength(1);
+
+            for (int i = 0; i < heght; i++)
             {
-                Console.WriteLine(myArray[i]);
+                for (int j = 0; j < width; j++)
+                {
+                    myArray[i, j] = random.Next(100);
+                }
             }
+
+            for (int i = 0; i < heght; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write(myArray[i, j] + "\t");
+                }
+
+                Console.WriteLine();
+            }
+
         }
     }
 }
