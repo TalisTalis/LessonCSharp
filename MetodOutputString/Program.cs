97ed33b12a4630f1ff83442810b77ccbb2c16b94
talistalis <tsarst@gmail.com>
2022-10-18 22:38:01 +0300

    методы

---
+++ MetodOutputString/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodOutputString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Написать метод, который выводит а экран строку.
+            // Символы, из которых состоит строка, и количество символов вводятся пользователем
+
+            Console.Write("Введите символ: ");
+
+            string str = Console.ReadLine();
+
+            Console.Write("Введите количество символов в строке: ");
+
+            int count = int.Parse(Console.ReadLine());
+
+            OutpuString(str, count);
+        }
+
+        static void OutpuString(string str, int count)
+        {
+            string result = "";
+
+            for (int i = 0; i < count; i++)
+            {
+                result += str;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ MetodSearchIndex/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodSearchIndex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Написать метод для поиска индекса массива (тип элементов в массиве - int)
+            // Метод должен вернуть индекс первого найденного элемента (если он будет найден)
+
+            int[] myArray = { 4, 5, 76, 79, 234, 8};
+
+            Console.Write("Введите искомое число в массиве: ");
+
+            int searchNumber = int.Parse(Console.ReadLine());
+
+            int res = FindIndexMassive(myArray, searchNumber);
+
+            if (res == -1)
+            {
+                Console.WriteLine("Данного элемента в массиве нет.");
+            }
+            else
+            {
+                Console.WriteLine($"Элемент {searchNumber} в массиве под индексом {res}");
+            }
+        }
+
+        static int FindIndexMassive(int[] metodArray, int element)
+        {
+            int findElement = -1;
+            
+            for (int i = 0; i < metodArray.Length; i++)
+            {
+                if (metodArray[i] == element)
+                {
+                    findElement = i;
+
+                    break;
+                }
+            }
+
+            return findElement;
+        }
+    }
+}
